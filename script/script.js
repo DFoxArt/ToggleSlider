8d15c0dd1ff379980d11ea022fbdf8924ae14d8b
FoxArt <torixcity@gmail.com>
2022-11-01 17:17:54 +0200

    Spaces Deleted, Project Finished

--- script/script.js
+++ script/script.js
@@ -1,5 +1,4 @@
 "use strict";
-
 const body = document.querySelector("body");
 const label = document.createElement("label");
 const input = document.createElement("input");
@@ -8,11 +7,9 @@ const slider = document.createElement("span");
 body.append(label);
 label.append(input);
 label.append(slider);
-
 label.classList.add("switch");
 input.type = "checkbox";
 slider.classList.add("slider", "round");
-
 input.addEventListener("change", () => {
   body.classList.toggle("dark");
 });
