35c1091d0f4f011cef70e900da78646384b7bc95
FoxArt <torixcity@gmail.com>
2022-11-01 17:18:24 +0200

    Spaces Deleted, Project Finished

--- script/script.js
+++ script/script.js
@@ -7,9 +7,11 @@ const slider = document.createElement("span");
 body.append(label);
 label.append(input);
 label.append(slider);
+
 label.classList.add("switch");
 input.type = "checkbox";
 slider.classList.add("slider", "round");
+
 input.addEventListener("change", () => {
   body.classList.toggle("dark");
 });
